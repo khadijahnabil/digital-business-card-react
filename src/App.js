3c96c982e24f56bf53577537dad34502c105e726
khadijahnabil <nabilkhadijah7@gmail.com>
2022-10-19 19:15:03 +0100

    created the CSS

--- src/App.js
+++ src/App.js
@@ -4,31 +4,46 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <img src="/profile-picture.jpeg" alt="Profile" width="250px" />
+        <img
+          src="/profile-picture.jpeg"
+          alt="Profile"
+          width="320px"
+          height="320px"
+          style={{ width: "320px", height: "320px", objectFit: "cover" }}
+        />
+        <h1>Khadijah Nabil</h1>
+        <p className="job-title">Frontend Developer</p>
+        <p className="website">kcodes.com</p>
+        <button type="button" className="btn btn-light big-links">
+          <i className="fa-solid fa-envelope"></i> Email
+        </button>
+        <button type="button" className="btn btn-primary big-links">
+          <i className="fa-brands fa-linkedin"></i> LinkedIn
+        </button>
+        <div>
+          <h2>About</h2>
+          <p className="info">
+            I am a frontend developer with an interest in making things simple
+            and automating daily tasks. I try to keep up with best practices and
+            always looking for new things to learn!
+          </p>
+        </div>
+        <div>
+          <h2>Interests</h2>
+          <p className="info">Content creation, Music, Food, Travel</p>
+        </div>
+        <footer>
+          <button type="button" className="btn btn-secondary small-links">
+            <i className="fa-brands fa-twitter" />
+          </button>
+          <button type="button" className="btn btn-secondary small-links">
+            <i className="fa-brands fa-instagram" />
+          </button>
+          <button type="button" className="btn btn-secondary small-links">
+            <i className="fa-brands fa-github" />
+          </button>
+        </footer>
       </div>
-      <h1>Khadijah Nabil</h1>
-      <p>Frontend Developer</p>
-      <p>kcodes.com</p>
-      <button>✉️ Email</button>
-      <button>LinkedIn</button>
-      <div>
-        <h2>About</h2>
-        <p>
-          Hello! My name is Khadijah and I am a frontend developer with an
-          interest in making things simple and automating daily tasks. I try to
-          keep up with best practices and always looking for new things to
-          learn!
-        </p>
-      </div>
-      <div>
-        <h2>Interests</h2>
-        <p>Content creation, Music, Food, Travel</p>
-      </div>
-      <footer>
-        <button>twitter</button>
-        <button>instagram</button>
-        <button>github</button>
-      </footer>
     </div>
   );
 }
